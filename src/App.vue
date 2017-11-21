2413b0830aef275ed8590278be2b989ce42c4072
Stas Kobzar <stas@modulis.ca>
2017-11-20 21:10:34 -0500

    Vuetify setup config start.

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,22 @@
 <template>
-  <b-container fluid id="app">
+  <v-app>
+    <v-navigation-drawer app fixed>
+    </v-navigation-drawer>
 
-    <b-row class="top-line">
-      <b-col>
-        <b-navbar variant="light" type="light">
-          Asterisk Queues Realtime Dashboard -
-          [{{ version }}]
-          <icon name="dot-circle-o" scale="1.6"
-            v-b-tooltip.hover title="Connected"
-            :class="['dot-circle', wsConnected ? 'connected' : 'disconnected' ]"/>
-        </b-navbar>
-      </b-col>
-    </b-row>
+    <v-toolbar color="indigo" clipped-left dark fixed app>
+      <v-toolbar-title>Asterisk Queues Realtime Dashboard</v-toolbar-title>
+    </v-toolbar>
 
-    <b-row>
-      <transition>
-        <b-col cols="2" class="ami-servers">
-          <AmiServers/>
-        </b-col>
-      </transition>
-      <b-col>
-        <b-row class="top-stats">
-          <b-col>
-            <TopStats/>
-          </b-col>
-        </b-row>
+    <v-content>
+      <v-container fluid>
+      </v-container>
+    </v-content>
 
-        <b-row>
-          <b-col>
-            <MenuPanel/>
-            <QueuesList/>
-          </b-col>
-          <b-col cols="4">
-            <QueueData/>
-          </b-col>
-        </b-row>
-      </b-col>
-    </b-row>
-    <b-modal title="Connection lost..." centered
-      :hide-footer="true"
-      :hide-header-close="true"
-      :no-close-on-backdrop="true"
-      :no-close-on-esc="true"
-      :visible="!wsConnected"
-      ref="wsconlost">
-      <b-row>
-        <b-col cols="3">
-          <icon name="exclamation-circle" scale="4" class="disconnected"/>
-        </b-col>
-        <b-col>
-          <p>Web-socket connection is lost.</p>
-          <p>
-            Trying to re-connect...
-            <div class="loader"></div>
-          </p>
-        </b-col>
-      </b-row>
-    </b-modal>
-    <b-modal title="Loading..." :data-loading="loading"
-      centered
-      :hide-footer="true"
-      :hide-header-close="true"
-      :no-close-on-backdrop="true"
-      :no-close-on-esc="true"
-      id="loadingModal"
-      ref="loadingModal">
-      <p>Loading queues...</p>
-    </b-modal>
-    <notifications width="400px" :max="10" group="main" position="top right">
-      <div slot="body" slot-scope="props" class="notification-main">
-        <div class="left-icon">
-          <icon name="check-circle-o" scale="1"/>
-        </div>
-        <div class="body">
-          <div class="notice">
-            {{ props.item.text }}
-          </div>
-        </div>
-        <div class="clearfix"></div>
-      </div>
-    </notifications>
-  </b-container>
+    <v-navigation-drawer right app fixed>
+    </v-navigation-drawer>
+
+    <v-footer color="indigo" dark fixed app></v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -119,46 +55,4 @@ export default {
 
 <style>
 #app {}
-.top-line {}
-.top-stats {margin: 15px 0; margin-left: -15px}
-.ami-servers { padding-top: 10px; background-color: #f8f9fa}
-.notification-main {
-  margin: 20px 20px 0px 0px;
-  border: 1px solid #866ab3;
-  border-left-size: 10px;
-  border-radius: 4px;
-  padding: 15px;
-  background-color: #EAF4FE;
-  box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.2);
-}
-.notification-main .left-icon {float: left; margin-right: 10px; color: green;}
-.notification-main .body{font-size: 13px}
-.connected{color: green}
-.disconnected{color: red}
-.loader {
-  height: 2px;
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-  background-color: #ddd;
-}
-.loader:before{
-  display: block;
-  position: absolute;
-  content: "";
-  left: -200px;
-  width: 200px;
-  height: 4px;
-  background-color: #2980b9;
-  animation: loading 2s linear infinite;
-}
-
-@keyframes loading {
-    from {left: -200px; width: 30%;}
-    50% {width: 30%;}
-    70% {width: 70%;}
-    80% { left: 50%;}
-    95% {left: 120%;}
-    to {left: 100%;}
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,40 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'bootstrap/dist/css/bootstrap.css'
-
-import Icon from 'vue-awesome/components/Icon'
-import 'vue-awesome/icons/clock-o'
-import 'vue-awesome/icons/phone-square'
-import 'vue-awesome/icons/user-circle'
-import 'vue-awesome/icons/user-circle-o'
-import 'vue-awesome/icons/server'
-import 'vue-awesome/icons/check'
-import 'vue-awesome/icons/pause-circle'
-import 'vue-awesome/icons/play-circle'
-import 'vue-awesome/icons/pause-circle-o'
-import 'vue-awesome/icons/play-circle-o'
-import 'vue-awesome/icons/users'
-import 'vue-awesome/icons/user'
-import 'vue-awesome/icons/volume-control-phone'
-import 'vue-awesome/icons/phone'
-import 'vue-awesome/icons/calendar-check-o'
-import 'vue-awesome/icons/hourglass-half'
-import 'vue-awesome/icons/hourglass-start'
-import 'vue-awesome/icons/arrow-circle-right'
-import 'vue-awesome/icons/balance-scale'
-import 'vue-awesome/icons/level-up'
-import 'vue-awesome/icons/exchange'
-import 'vue-awesome/icons/check-circle-o'
-import 'vue-awesome/icons/chain-broken'
-import 'vue-awesome/icons/exclamation-circle'
-import 'vue-awesome/icons/dot-circle-o'
-
-import Notifications from 'vue-notification'
-
+import 'vuetify/dist/vuetify.min.css'
 import App from './App'
 import WebSock from './websock'
 import store from './store'
@@ -42,11 +11,8 @@ import './filters'
 
 Vue.config.productionTip = false
 
-Vue.use(BootstrapVue)
+Vue.use(Vuetify)
 Vue.use(WebSock, store)
-Vue.use(Notifications)
-
-Vue.component('icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
